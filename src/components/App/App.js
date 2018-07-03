8409c586ea7ec5988c513b3e3b247c80fa4effc2
dimkawp <dimkawp@gmail.com>
2018-07-03 12:34:16 +0300

    Create project
    - install react
    - build directory dom
    - add sass
    - add normalize css
    - clean readme

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Home from '../Main/Home';
+import 'normalize.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Home />
+      </div>
+    );
+  }
+}
+
+export default App;
